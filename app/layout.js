0e717f04ec3a328c0e9b2f119585397769adbc8e
Yaniv <yanivlm@gmail.com>
2026-07-07 12:55:23 +0300

    fix GTM container ID

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N43LNDPH"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TVWCZL96"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -84,7 +84,7 @@ try{
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N43LNDPH');
+})(window,document,'script','dataLayer','GTM-TVWCZL96');
 `}
         </Script>
         {/* End Google Tag Manager */}
